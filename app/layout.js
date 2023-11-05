a963bc17f1dc773dbfd385f6ec29f209da5b6a0b
Parthiv-Sarker <parthivsdevelopment@gmail.com>
2023-11-05 14:57:01 +0530

    add timer

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,23 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import StarsCanvas from '@/components/Home/StarBackground'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import StarsCanvas from "@/components/Home/StarBackground";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'TechVerse',
-  description: 'Welcome to techverse',
-}
+	title: "TechVerse",
+	description: "Welcome to techverse",
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
-      <StarsCanvas/>
-        {children}
-        </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body
+				className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+			>
+				<StarsCanvas />
+				{children}
+			</body>
+		</html>
+	);
 }
--- app/page.js
+++ app/page.js
@@ -1,15 +1,16 @@
 import Hero from "@/components/Home/Hero";
-import Image from "next/image";
+import Timer from "@/components/Timer/Timer";
 
 export default function Home() {
 	return (
 		<main className="h-screen w-screen">
 			<div className="flex flex-col justify-center items-center">
 				<Hero />
-				<h1 className=" absolute text-box font-young text-4xl sm:text-5xl md:text-6xl lg:text-9xl">
+				<h1 className="absolute text-box font-young text-4xl sm:text-5xl md:text-6xl lg:text-9xl">
 					TECHVERSE 2.O
 				</h1>
 			</div>
+			<Timer />
 		</main>
 	);
 }
